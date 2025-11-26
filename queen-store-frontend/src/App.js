9ec00d4392bb7606be061cafb5009f601edfa223
wesley <wesleydejesusalvarenga@gmail.com>
2025-11-25 22:35:46 -0300

    add monitoramento

--- queen-store-frontend/src/App.js
+++ queen-store-frontend/src/App.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useState, createContext, useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import Carrinho from './pages/Carrinho';
 import ProdutoDetalhe from './pages/ProdutoDetalhe';
