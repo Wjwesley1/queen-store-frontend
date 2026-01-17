99614f8dadafdb5025a982d2bf615923263768fa
Wjwesley1 <wesleydejesusalvarenga@gmail.com>
2026-01-17 19:37:14 -0300

    update

--- queen-store-frontend/src/pages/Carrinho.js
+++ queen-store-frontend/src/pages/Carrinho.js
@@ -1,6 +1,6 @@
 // src/pages/Carrinho.jsx — VERSÃO FINAL COM INTEGRAÇÃO DE DADOS LOGADO + CONFIRMAÇÃO RÁPIDA
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useCarrinho } from '../App';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
